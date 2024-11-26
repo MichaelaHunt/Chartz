a031bf0604b9574ef7dce5b038c152f5efebec03
ElBoyTM <nszalenski@gmail.com>
2024-11-26 13:45:39 -0500

    made iTunesRoutes

--- chartz/server/src/routes/iTunesRoutes.ts
+++ chartz/server/src/routes/iTunesRoutes.ts
@@ -1,1 +1,19 @@
-const searchITunes = async (query: string) => {};
+const searchITunes = async () => {
+    try {
+        const response = await fetch(
+        `https://itunes.apple.com/us/rss/topsongs/limit=10/json`
+        );
+        console.log('Response:', response);
+        const data = await response.json();
+        if (!response.ok) {
+        throw new Error('invalid API response, check the network tab');
+        }
+        console.log('Data:', data);
+        return data;
+    } catch (err) {
+        console.log('an error occurred', err);
+        return [];
+    }
+};
+
+export { searchITunes };
