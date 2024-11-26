8916f50ce4817acb571d404517ce0aedb619c71b
Apgash <ashtonvilkosi@gmail.com>
2024-11-25 21:28:59 -0500

    adds code to user.ts and saved songs,ts

--- chartz/server/src/models/user.ts
+++ chartz/server/src/models/user.ts
@@ -1,32 +1,33 @@
-// import { DataTypes, Sequelize, Model, Optional } from 'sequelize';
+import { DataTypes, Sequelize, Model} from 'sequelize';
 
-// export class User {
-//     public id!: number;
-//     public email!: string;
-//     public password!: string;
-// }
+export class User extends Model {
+    public id!: number;
+    public username!: string;
+    public password!: string;
+}
 
-// export function UserFactory(sequelize: Sequelize): typeof User {
-//     User.init(
-//         {
-//             id: {
-//                 type: DataTypes.INTEGER,
-//                 autoIncrement: true,  
-//                 primaryKey: true,     
-//             },
-//             email: {
-//                 type: DataTypes.STRING,
-//                 allowNull: false,
-//             },
-//             password: {
-//                 type: DataTypes.STRING,
-//                 allowNull: false,
-//             }
-//         },
-//         {
-//             tableName: 'users',
-//             sequelize,
-//         }
-//     )
-//     return User;
-// }
+export function UserFactory(sequelize: Sequelize): typeof User {
+    User.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,  
+                primaryKey: true,     
+            },
+            username: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+                unique: true, 
+            },
+            password: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+            }
+        },
+        {
+            tableName: 'users',
+            sequelize,
+        }
+    )
+    return User;
+}
