3e7fba4d51a71a24ce8caa2008d59586d387530e
Daniel T <Danieltwarg@gmail.com>
2024-11-26 21:26:50 -0500

    create class to authorize user that handles login, logout, and decode

--- chartz/client/src/utils/auth.ts
+++ chartz/client/src/utils/auth.ts
@@ -1,2 +1,31 @@
 import { type JwtPayload, jwtDecode } from 'jwt-decode';
 import type { UserLogin } from '../interfaces/UserLogin';
+
+class AuthorizeUser {
+    getProfile() {
+        const token = this.getToken();
+        if (!token) {
+            throw new Error('No profile found!');
+        }
+        return jwtDecode<UserLogin>(token);
+    }
+
+    loggedIn() {
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+    }
+//create a function to check if the token is expired.
+    isTokenExpired(token: string) {
+        try {
+            //try to decode the token and expect the token JwtPayLoad type.
+            const decodedToken = jwtDecode<JwtPayload>(token);
+
+            //check to see if the token is expired, if so return true.
+            if (decodedToken?.exp && decodedToken?.exp < Date.now() / 1000) {
+                return true;
+            }
+        } catch (error) {
+            //if decoding fails, return false.
+            return false;
+        }
+    }
