cd3a5857dac0315f73fb0880067f5cc0c2ec54eb
Michaela Hunt <mhunt@theprovatogroup.com>
2024-11-30 14:36:07 -0500

    fixed discrepancy between login and home pages css

--- chartz/client/src/pages/Login.tsx
+++ chartz/client/src/pages/Login.tsx
@@ -5,16 +5,20 @@ import './Pages.css';
 function Login() {
     return (
         <>
-            <div className="body">
-                {/* <Navbar></Navbar> */}
-                <div className="loginContainer">
-                    <h1 className="loginTitle">Log in to Chartz</h1>
-                    <TextField></TextField>
-                    <TextField></TextField>
-                    <p className="error">Incorrect login. Please try again.</p> {/*visibility: hidden*/}
-                    <button>Login</button>
-                    <p>or</p>
-                    <button>Sign Up</button>
+            <div className="page">
+                <Navbar></Navbar>
+                <div className="body">
+                    <div className="loginContainer">
+                        <div className="loginItem">
+                            <h1 className="loginTitle">Log in to Chartz</h1>
+                            <TextField></TextField>
+                            <TextField></TextField>
+                            <p className="error">Incorrect login. Please try again.</p> {/*visibility: hidden*/}
+                            <button>Login</button>
+                            <p>or</p>
+                            <button>Sign Up</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
