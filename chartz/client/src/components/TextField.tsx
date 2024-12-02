1bc8daade738177d3ed34d1e32b72a06593ef5ea
Daniel T <Danieltwarg@gmail.com>
2024-12-02 16:22:25 -0500

    added type and set up Textfield Component

--- chartz/client/src/components/TextField.tsx
+++ chartz/client/src/components/TextField.tsx
@@ -1,11 +1,24 @@
 import "./Components.css";
 
-function TextField() {
+type TextFieldProps = {
+    label: string;
+    value: string;
+    onChange: (value: string) => void;
+    errorMessage?: string;
+}
+
+function TextField({label, value, onChange, errorMessage}: TextFieldProps) {
     return(
         <div className="textField">
-            <h3>Email Address</h3>
-            <input className="loginInput"></input>
-        </div>
+        <h3>{label}</h3>
+        <input
+          className="loginInput"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          type={label === "Password" ? "password" : "text"}
+        />
+        {errorMessage && <p className="error">{errorMessage}</p>}
+      </div>
     );
 }
 
