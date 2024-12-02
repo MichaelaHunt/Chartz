d5c96b5fb1c1d5874118140fbc4caef7401dfb97
Daniel T <Danieltwarg@gmail.com>
2024-12-02 16:36:05 -0500

    removed incorrect code in textfield

--- chartz/client/src/components/TextField.tsx
+++ chartz/client/src/components/TextField.tsx
@@ -1,24 +1,11 @@
 import "./Components.css";
 
-type TextFieldProps = {
-    label: string;
-    value: string;
-    onChange: (value: string) => void;
-    errorMessage?: string;
-}
-
-function TextField({label, value, onChange, errorMessage}: TextFieldProps) {
+function TextField() {
     return(
         <div className="textField">
-        <h3>{label}</h3>
-        <input
-          className="loginInput"
-          value={value}
-          onChange={(e) => onChange(e.target.value)}
-          type={label === "Password" ? "password" : "text"}
-        />
-        {errorMessage && <p className="error">{errorMessage}</p>}
-      </div>
+            <h3>Email Address</h3>
+            <input className="loginInput"></input>
+        </div>
     );
 }
 
