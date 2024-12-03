d3ea723168ae0808540824b8f3836b263c688144
Daniel T <Danieltwarg@gmail.com>
2024-12-03 17:56:02 -0500

    changes to auth

--- chartz/server/src/routes/index.ts
+++ chartz/server/src/routes/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import apiRouter from './api/index';
 const router = express.Router();
 import userRouter from './user-routes';
-import authRouter from './auth-routes2';
+import authRouter from './auth-routes';
 
 router.use('/api', apiRouter);
 router.use('/users', userRouter);
