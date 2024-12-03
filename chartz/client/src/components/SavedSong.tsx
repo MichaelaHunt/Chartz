676614853142c3c1c047cdf8ce3e434f84b9588e
Apgash <ashtonvilkosi@gmail.com>
2024-12-02 21:15:13 -0500

    adds starter code to remove a saved song

--- chartz/client/src/components/SavedSong.tsx
+++ chartz/client/src/components/SavedSong.tsx
@@ -1,10 +1,13 @@
+import "./Components.css";
+
 interface SavedSongProps {
     img: string;
     title: string;
     artist: string;
+    onRemove: () => void;
 }
 
-function SavedSong({ img, title, artist }: SavedSongProps) {
+function SavedSong({ img, title, artist, onRemove }: SavedSongProps) {
     return (
         <>
             <div className="savedSongItem">
@@ -12,6 +15,7 @@ function SavedSong({ img, title, artist }: SavedSongProps) {
                 <h3>{title}</h3>{/*in the code, truncate the song name if it gets too long we want to leave room for the artist name! */}
                 <h3 className="spacer">|</h3>
                 <h3>{artist}</h3>
+                <button onClick={onRemove} className="removeButton"><i className="fa-solid fa-trash"></i></button>
             </div>
         </>
     );
