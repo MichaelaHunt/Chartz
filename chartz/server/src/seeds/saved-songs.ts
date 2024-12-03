49188465d3f2add7821adad04c393ab5e495642b
ElBoyTM <nszalenski@gmail.com>
2024-12-03 18:53:57 -0500

    reworked saved songs

--- chartz/server/src/seeds/saved-songs.ts
+++ chartz/server/src/seeds/saved-songs.ts
@@ -3,20 +3,12 @@ import { SavedSongModel } from "../models";
 export const seedSavedSongs = async () => {
   await SavedSongModel.bulkCreate([
     {
-      song_id: "1",
-      song_name: "Sandstorm",
-      artist: "Darude",
-      album: "Before the Storm",
-      genre: "Trance",
-      user_id: "1",
+      geniusSongId: 2993,
+      songTitle: "All Star",
     },
     {
-      song_id: "2",
-      song_name: "Song 2",
-      artist: "Blur",
-      album: "Blur",
-      genre: "Alternative",
-      user_id: "2",
+      geniusSongId: 83049,
+      songTitle: "Accidentally in Love",
     },
   ]);
 };
