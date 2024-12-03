adf2acf66c889117911896d5e7b05cec94001632
Daniel T <Danieltwarg@gmail.com>
2024-12-02 21:31:13 -0500

    changed props and added functionality to login

--- chartz/client/src/components/TextField.tsx
+++ chartz/client/src/components/TextField.tsx
@@ -1,10 +1,24 @@
 import "./Components.css";
 
-function TextField({inputLabel}: {inputLabel: string}) {
-    return(
+interface TextFieldProps {
+    inputLabel: string | null;
+    name: string | null;
+    value: string | null;
+    type?: string | null; // Optional, defaults to "text"
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function TextField({ inputLabel, name, value, type = "text", onChange }: TextFieldProps) {
+    return (
         <div className="textField">
             <h3>{inputLabel}</h3>
-            <input className="loginInput"></input>
+            <input
+                className="loginInput"
+                type={type}
+                name={name}
+                value={value}
+                onChange={onChange}
+            />
         </div>
     );
 }
