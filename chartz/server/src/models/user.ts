c99e14c12c761dc94e967ee11c350d794d4f22c3
Daniel T <Danieltwarg@gmail.com>
2024-12-02 20:36:19 -0500

    added auth-routes, added to user.ts for hashing, added login method for hashing

--- chartz/server/src/models/user.ts
+++ chartz/server/src/models/user.ts
@@ -1,10 +1,31 @@
-import { DataTypes, Sequelize, Model} from 'sequelize';
+import { DataTypes, Sequelize, Model, type Optional } from 'sequelize';
+import bcrypt from 'bcrypt';
 
-export class User extends Model {
+interface UserAttributes {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+
+export class User extends Model<UserAttributes, UserCreationAttributes> 
+implements UserAttributes {
     public id!: number;
     public username!: string;
     public password!: string;
-}
+    public email!: string;
+
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+
+    public async hashPassword(password: string) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(password, saltRounds);
+    }
+    }
+
 
 export function UserFactory(sequelize: Sequelize): typeof User {
     User.init(
@@ -19,15 +40,29 @@ export function UserFactory(sequelize: Sequelize): typeof User {
                 allowNull: false,
                 unique: true, 
             },
+            email: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+                unique: true,
+            },
             password: {
                 type: DataTypes.STRING(50),
                 allowNull: false,
-            }
+            },
         },
         {
             tableName: 'users',
             sequelize,
+            hooks: {
+                beforeCreate: async (user: User) => {
+                    await user.hashPassword(user.password);
+                },
+                beforeUpdate: async (user: User) => {
+                    await user.hashPassword(user.password);
+                },
+            },
         }
-    )
+    );
+    
     return User;
 }
