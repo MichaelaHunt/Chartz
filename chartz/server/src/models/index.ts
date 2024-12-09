5c1b1c7f4b5383e655bbb873fbcd40d738af4091
Ross Ritchey <ross@assistedinnovations.com>
2024-12-09 14:54:48 -0500

    Rework database a bit

--- chartz/server/src/models/index.ts
+++ chartz/server/src/models/index.ts
@@ -1,38 +1,13 @@
 import sequelize from "../config/connection.js";
 import { UserFactory } from "./user.js";
 import { SavedSongFactory } from "./savedSong.js";
-import { DataTypes } from "sequelize";
 
 const UserModel = UserFactory(sequelize);
 
 const SavedSongModel = SavedSongFactory(sequelize);
 
-// Define the tables 
-const UserSavedSong = sequelize.define(
-    "UserSavedSong",
-    {
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: UserModel,
-          key: "id",
-        },
-      },
-      savedSongId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: SavedSongModel,
-          key: "id",
-        },
-      },
-    },
-    { timestamps: false, tableName: "userSongs" }
-  );
-
 //set up associations
-UserModel.belongsToMany(SavedSongModel, { through: UserSavedSong, foreignKey: 'userId' });
-SavedSongModel.belongsToMany(UserModel, { through: UserSavedSong, foreignKey: 'savedSongId' });
+UserModel.belongsToMany(SavedSongModel, { through: 'UserSavedSong' });
+SavedSongModel.belongsToMany(UserModel, { through: 'UserSavedSong' });
 
-export { UserModel, SavedSongModel, UserSavedSong };
+export { UserModel, SavedSongModel };
