7a58c12ea927249280dcfc6a6122608ff78c8c16
Michaela Hunt <mhunt@theprovatogroup.com>
2024-12-09 14:41:46 -0500

    Fix the Top3Item change

--- chartz/client/src/components/Top3Item.tsx
+++ chartz/client/src/components/Top3Item.tsx
@@ -6,30 +6,28 @@ interface Top3ItemProps {
     title: string;
     artist: string;
     rank: number;
-    onClick: () => void; // onClick prop
 }
 
-function Top3Item({ img, title, artist, rank, onClick }: Top3ItemProps) {
+function Top3Item({ img, title, artist, rank }: Top3ItemProps) {
     const [songTitle, setSongTitle] = useState<string>(title);
-
     useEffect(() => {
-        // Truncate the title to necessary length
+        //truncate the title to necessary length
         let tempTitle = title;
         let authorMark = " - " + artist;
         if (title.includes(authorMark)) {
-            tempTitle = title.slice(0, title.length - authorMark.length);
+            tempTitle = title.slice(0, (title.length - authorMark.length));
         }
-        // Remove any (feat. ****)
+        //Then let's remove any (feat. ****)
         tempTitle = tempTitle.replace(/\s?\(.*?\)\s?/g, '');
         if (tempTitle.length > 52) {
-            tempTitle = tempTitle.slice(0, 50) + "...";
+            tempTitle = title.slice(0, 50) + "...";
         }
 
         setSongTitle(tempTitle);
-    }, [title, artist]);
-
+        
+    }, []);
     return (
-        <div className="top3item" onClick={onClick} style={{ cursor: "pointer" }}>
+        <div className="top3item">
             <div id="bringToDetailsPg">
                 <img src={img} alt={songTitle} className="top3img" />
                 <h6 className="top3title">{songTitle}</h6>
