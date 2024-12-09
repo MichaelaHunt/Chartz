2208845200b8a19a2640a1dad17fc561dce175e0
Michaela Hunt <mhunt@theprovatogroup.com>
2024-12-09 13:33:19 -0500

    Change the port to the DB port

--- chartz/client/vite.config.ts
+++ chartz/client/vite.config.ts
@@ -7,17 +7,17 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:5432',
         changeOrigin: true,
         secure: false,
       },
       '/auth': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:5432',
         changeOrigin: true,
         secure: false,
       },
       '/users': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:5432',
         changeOrigin: true,
         secure: false,
       },
